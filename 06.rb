d34cd72abb9a3630a7b9235ca9d3008ef3960293
uchiyu <s13t208@stmail.eng.kagawa-u.ac.jp>
2016-01-31 23:48:35 +0900

    add 06

--- 06.rb
+++ 06.rb
@@ -1,18 +1,13 @@
-def char_bi_gram(text)
-  arr = Array.new(text.length) { Array.new(text.length) }
+def char_ngram(text, n)
+  arr = Array.new(text.length)
   str = text.gsub(' ', '')
   i = 0
   str.each_char do |ch|
-    if i == 0
-      arr[i][1] = ch[i]
-      arr[i][2] = ch[i+1]
-    elsif i == arr.length-1
-      arr[i][0] = ch[i-1]
-      arr[i][1] = ch[i]
-    else
-      arr[i][0] = ch[i-1]
-      arr[i][1] = ch[i]
-      arr[i][2] = ch[i+1]
+    arr[i] = ''
+    for num in i-(n-1)..i+(n-1)
+      if ( str[num] != nil )
+        arr[i] << str[num]
+      end
     end
     i = i + 1
   end
@@ -22,6 +17,16 @@ end
 text1 = "paraparaparadise"
 text2 = "paragraph"
 
-arr1 = char_bi_gram(text1)
+arr1 = char_ngram(text1, 2)
+arr2 = char_ngram(text2, 2)
 
-
+# 各要素
+p arr1
+p arr2
+puts
+# 和集合
+p arr1 | arr2
+# 差集合
+p arr1 - arr2
+# 積集合
+p arr1 & arr2
