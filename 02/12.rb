beb2f4056a2211e259e4cd1f8c549eee8e6d3a88
uchiyu <s13t208@stmail.eng.kagawa-u.ac.jp>
2016-02-04 15:24:58 +0900

    fix file output

--- 02/12.rb
+++ 02/12.rb
@@ -1,18 +1,19 @@
-text = ''
-i = 0
+text1 = ''
+text2 = ''
 File.open('hightemp.txt') do |file|
   file.each do |line|
-      text << line.to_s.split(/\t/)[0] + "\n"
+      text1 << line.to_s.split(/\t/)[0] + "\n"
+      text2 << line.to_s.split(/\t/)[1] + "\n"
   end
 end
+File.write('col1.txt', text1 )
+File.write('col2.txt', text2 )
 
-ans = %x(cut -f 1 hightemp.txt)
+ans1 = %x(cut -f 1 hightemp.txt)
+ans2 = %x(cut -f 2 hightemp.txt)
 
-if text == ans
+if ans1 == File.read('col1.txt') && ans2 == File.read('col2.txt')
   puts 'Success'
-  puts text
 else
   puts 'Failed'
-  puts text
-  puts ans
 end
