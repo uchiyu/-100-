e30456022eee4c88a8ae7b51ff241f2c945a9f87
uchiyu <s13t208@stmail.eng.kagawa-u.ac.jp>
2016-02-16 14:36:28 +0900

    fix 27

--- 03/27.rb
+++ 03/27.rb
@@ -10,7 +10,8 @@ open(json_file_path).each do |full_data|
   full_basic_data = full_basic_data.to_s.gsub(/({{基礎情報).*?\|/, '').delete('\\n}}\\n')
   full_basic_data.to_s.split('|').each do |data|
     line_data = data.to_s.partition('=')
-    basic_data[line_data[0]] = line_data[2].to_s.delete('\\\\').gsub(/[']{2,5}/, '').gsub(/\[+/, '').gsub(/\]+/, '')
+    tmp = line_data[2].to_s.delete('\\\\').gsub(/[']{2,5}/, '')
+    basic_data[line_data[0].to_s.strip] = tmp.to_s.gsub(/[\[]+/, '').gsub(/[\]]+/, '')
   end
 end
 
