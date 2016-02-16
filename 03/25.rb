204d3503f4017c9ded6d35d6b10de1421f1c2f32
uchiyu <s13t208@stmail.eng.kagawa-u.ac.jp>
2016-02-16 14:59:30 +0900

    fix taking basic data

--- 03/25.rb
+++ 03/25.rb
@@ -8,9 +8,10 @@ line_data = Array.new
 open(json_file_path).each do |full_data|
   full_basic_data = full_data.match(/({{基礎情報).*(\\n}}\\n)/)
   full_basic_data = full_basic_data.to_s.gsub(/({{基礎情報).*?\|/, '').delete('\\n}}\\n')
-  full_basic_data.to_s.split('|').each do |data|
+  full_basic_data.to_s.split('\|').each do |data|
     line_data = data.to_s.partition('=')
-    basic_data[line_data[0]] = line_data[2].to_s.delete('\\\\')
+    basic_data[line_data[0].to_s.strip] = line_data[2].to_s.delete('\\\\')
+    puts
   end
 end
 
--- 03/26.rb
+++ 03/26.rb
@@ -8,8 +8,8 @@ line_data = Array.new
 open(json_file_path).each do |full_data|
   full_basic_data = full_data.match(/({{基礎情報).*(\\n}}\\n)/)
   full_basic_data = full_basic_data.to_s.gsub(/({{基礎情報).*?\|/, '').delete('\\n}}\\n')
-  full_basic_data.to_s.split('|').each do |data|
-    line_data = data.to_s.partition('=')
+  full_basic_data.each_line do |data|
+    line_data = data.to_s.delete('\|').partition('=')
     basic_data[line_data[0]] = line_data[2].to_s.delete('\\\\').gsub(/[']{2,5}/, '')
   end
 end
