ddd9cf8544894b2b68ae16e9aaf7a91bdd27e65e
uchiyu <s13t208@stmail.eng.kagawa-u.ac.jp>
2016-02-23 13:00:17 +0900

    add 33

---
+++ 04/33.rb
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+require 'natto'
+
+def read_mecab(file_name)
+  mecab_data = Array.new
+  data = Array.new
+  open(file_name).each do |line|
+    if line =~ /EOS/
+      mecab_data.push(data)
+      data = Array.new
+      next
+    end
+    line_data = line.chomp.split(/\t/)
+    surface = line_data[0]
+    block = line_data[1].split(',')
+    data.push({:surface=>surface, :base=>block[6], :pos=>block[0], :pos1=>block[1]})
+  end
+  return mecab_data
+end
+
+name = 'neko.txt.mecab'
+mecab_data = read_mecab(name)
+
+mecab_data.each do | block_data |
+  block_data.each do | data |
+    if data[:pos1] == 'サ変接続' && data[:pos] == '名詞'
+      puts data[:base]
+    end
+  end
+end
